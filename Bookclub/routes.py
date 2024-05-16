82e7b3a81868b02fac22d446b989bd56c12139ff
Ella Zhang <23844446@student.uwa.edu.au>
2024-05-16 16:16:28 +0800

    Implement blueprint in the routes.py
    change each url_for

--- Bookclub/routes.py
+++ Bookclub/routes.py
@@ -1,8 +1,8 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request, abort
-from Bookclub import app, db, bcrypt
+from flask import Blueprint,render_template, url_for, flash, redirect, request, abort
+from Bookclub import db, bcrypt
 from Bookclub.forms import (
     RegistrationForm, LoginForm, UpdateAccountForm, PostForm, SearchForm,
     CommentForm, EmptyForm, FollowForm, UnfollowForm
@@ -13,12 +13,14 @@ from werkzeug.utils import secure_filename
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
+main = Blueprint('main', __name__)
+
 # Home page route: Displays the home page
-@app.route("/", methods=['GET'])
+@main.route("/", methods=['GET'])
 def home():
     return render_template('home.html', title='Home')
 
-@app.route('/user/<username>')
+@main.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
@@ -38,7 +40,7 @@ def user(username):
 
 
 # Forum page route: Displays all posts on the forum page
-@app.route("/forum")
+@main.route("/forum")
 def forum():
     # Add Pagination
     page = request.args.get('page', 1, type=int)
@@ -46,7 +48,7 @@ def forum():
     return render_template('forum.html', posts=posts)
 
 # Search books route: Allows searching books by title, genre, author, status, or ISBN
-@app.route("/search_books", methods=['GET', 'POST'])
+@main.route("/search_books", methods=['GET', 'POST'])
 def search_books():
     results = []
     if request.method == 'POST':
@@ -68,10 +70,10 @@ def search_books():
     return render_template('search_books.html')
      
 #User registration route handling GET and POST requests, implements registration functionality
-@app.route("/register", methods=['GET', 'POST'])
+@main.route("/register", methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('main.home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -79,30 +81,30 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
+        return redirect(url_for('main.login'))
     return render_template('register.html', title='Register', form=form)
 
 # Login route for handling user logins with form validation and session management
-@app.route("/login", methods=['GET', 'POST'])
+@main.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('main.home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 # Logout route to handle user session termination
-@app.route("/logout")
+@main.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('main.home'))
 
 
 def upload_images(form_picture, storage_path, output_size=(125, 125)):
@@ -110,7 +112,7 @@ def upload_images(form_picture, storage_path, output_size=(125, 125)):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
 
-    picture_path = os.path.join(app.root_path, storage_path, picture_fn)
+    picture_path = os.path.join(main.root_path, storage_path, picture_fn)
 
     i = Image.open(form_picture)
     i.thumbnail(output_size)  # Resize the image using the provided output_size
@@ -122,7 +124,7 @@ def upload_images(form_picture, storage_path, output_size=(125, 125)):
     return picture_fn
 
 # Update last_seen before each request for authenticated users
-@app.before_request
+@main.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.now(ZoneInfo("Australia/Perth"))
@@ -130,7 +132,7 @@ def before_request():
         db.session.commit()
 
 # Profile update route, requires login to access
-@app.route("/profile", methods=['GET', 'POST'])
+@main.route("/profile", methods=['GET', 'POST'])
 @login_required
 def profile():
     form = UpdateAccountForm()
@@ -145,7 +147,7 @@ def profile():
         print(f"Updating profile: {current_user.last_seen}")
         db.session.commit()
         flash('Your account has been updated!', 'success')
-        return redirect(url_for('profile'))
+        return redirect(url_for('main.profile'))
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
@@ -156,7 +158,7 @@ def profile():
 
 
 # Route to create a new post, requires login to access
-@app.route("/post/new", methods=['GET', 'POST'])
+@main.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
     form = PostForm()
@@ -169,13 +171,13 @@ def new_post():
         db.session.add(post)
         db.session.commit()
         flash('Your post has been created!', 'success')
-        return redirect(url_for('forum'))
+        return redirect(url_for('main.forum'))
     return render_template('create_post.html', title='New Post',
                            form=form, legend='New Post')
 
 #comments     
 
-@app.route("/post/<int:post_id>", methods=['GET', 'POST'])
+@main.route("/post/<int:post_id>", methods=['GET', 'POST'])
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     allComments = Comment.query.filter_by(to_post_id=post_id).all()
@@ -213,11 +215,11 @@ def post(post_id):
         db.session.add(comment)
         db.session.commit()
         flash('Your comment has been created!', 'success')
-        return redirect(url_for('post', post_id=post_id))
+        return redirect(url_for('main.post', post_id=post_id))
 
     return render_template('post.html', postTitle=post.title, post=post, form=form, legend='New comment', comments=allComments)
 
-@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@main.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -232,7 +234,7 @@ def update_post(post_id):
         post.content = form.content.data
         db.session.commit()
         flash('Your post has been updated!', 'success')
-        return redirect(url_for('post', post_id=post.id))
+        return redirect(url_for('main.post', post_id=post.id))
     elif request.method == 'GET':
         form.title.data = post.title
         form.content.data = post.content
@@ -240,7 +242,7 @@ def update_post(post_id):
     return render_template('update_post.html', title='Update Post', form=form, legend='Update Post', image_file=image_file)
 
 # Route to delete an existing post, requires login and user must be the author
-@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@main.route("/post/<int:post_id>/delete", methods=['POST'])
 @login_required
 def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -249,11 +251,11 @@ def delete_post(post_id):
     db.session.delete(post)
     db.session.commit()
     flash('Your post has been deleted!', 'success')
-    return redirect(url_for('forum'))
+    return redirect(url_for('main.forum'))
 
 
 #follow  route
-@app.route('/follow/<username>', methods=['POST'])
+@main.route('/follow/<username>', methods=['POST'])
 @login_required
 def follow(username):
     form = EmptyForm()
@@ -261,16 +263,16 @@ def follow(username):
         user = User.query.filter_by(username=username).first_or_404()
         if user == current_user:
             flash('You cannot follow yourself!', 'danger')
-            return redirect(url_for('user', username=username))
+            return redirect(url_for('main.user', username=username))
         current_user.follow(user)
         db.session.commit()
         flash(f'You are now following {username}!', 'success')
-        return redirect(url_for('user', username=username))
+        return redirect(url_for('main.user', username=username))
     else:
-        return redirect(url_for('forum'))
+        return redirect(url_for('main.forum'))
     
 
-@app.route('/unfollow/<username>', methods=['POST'])
+@main.route('/unfollow/<username>', methods=['POST'])
 @login_required
 def unfollow(username):
     form = EmptyForm()
@@ -278,16 +280,16 @@ def unfollow(username):
         user = User.query.filter_by(username=username).first_or_404()
         if user == current_user:
             flash('You cannot unfollow yourself!', 'danger')
-            return redirect(url_for('user', username=username))
+            return redirect(url_for('main.user', username=username))
         current_user.unfollow(user)
         db.session.commit()
         flash(f'You are no longer following {username}.', 'success')
-        return redirect(url_for('user', username=username))
+        return redirect(url_for('main.user', username=username))
     else:
-        return redirect(url_for('forum'))
+        return redirect(url_for('main.forum'))
 
 # Like route
-@app.route("/post/<int:post_id>/like", methods=['POST'])
+@main.route("/post/<int:post_id>/like", methods=['POST'])
 @login_required
 def like_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -301,17 +303,17 @@ def like_post(post_id):
         db.session.add(new_like)
         db.session.commit()
         flash('You liked the post!', 'success')
-    return redirect(url_for('post', post_id=post_id))
+    return redirect(url_for('main.post', post_id=post_id))
 
 #pass stuff to NavBar 
-@app.context_processor
+@main.context_processor
 def layout():
     form = SearchForm()
     return dict(form=form)
 
 
 # Create search function 
-@app.route('/search', methods=["GET", "POST"])
+@main.route('/search', methods=["GET", "POST"])
 def search():
     form = SearchForm()
     posts = Post.query
@@ -335,7 +337,7 @@ def search():
                            posts=[])
 
 # bookshelf
-@app.route("/shelf", methods=['GET', 'POST'])
+@main.route("/shelf", methods=['GET', 'POST'])
 @login_required
 def shelf(): 
     if request.method == 'POST':
