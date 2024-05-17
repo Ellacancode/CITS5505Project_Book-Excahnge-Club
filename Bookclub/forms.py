d3e971e007adf7598160ea05bb183c82dedf123e
lucychen0305 <164651593+lucychen0305@users.noreply.github.com>
2024-05-17 22:58:06 +0800

    updating reset password form

--- Bookclub/forms.py
+++ Bookclub/forms.py
@@ -101,26 +101,7 @@ class FollowForm(FlaskForm):
 class UnfollowForm(FlaskForm):
     submit = SubmitField('Unfollow', validators=[DataRequired()])
 
-<<<<<<< HEAD
 class ResetPasswordForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField("Reset Password")
-=======
 
-class RequestResetForm(FlaskForm):
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is None:
-            raise ValidationError('There is no account with that email. You must register first.')
-
-
-class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Reset Password')
->>>>>>> 430c090baf663a5c62aa49dc88a4921fb0bdf5ba
