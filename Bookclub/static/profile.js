b25f95daa3bd40bcd62d208f3260366ffdba431d
lucychen0305 <164651593+lucychen0305@users.noreply.github.com>
2024-05-18 16:43:38 +0800

    adding comments to login, post, profile page

--- Bookclub/static/profile.js
+++ Bookclub/static/profile.js
@@ -1,8 +1,9 @@
+// This script enables live preview of uploaded images and manages associated resources.
 document.getElementById('imageUpload').addEventListener('change', function(event) {
     var output = document.getElementById('imagePreview');
     output.style.display = 'block';
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function() {
-        URL.revokeObjectURL(output.src)
+        URL.revokeObjectURL(output.src);
     }
 });
